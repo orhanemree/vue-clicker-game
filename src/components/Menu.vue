7a4c70af3e843b56bfb9dc14cd1a7241c0a637ce
Orhan Emre <orhanemre.dev@gmail.com>
2021-12-02 20:35:19 +0300

    Update

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="menu flex">
     <li class="flex">
-      <a href="https://github.com/orhanemree"><img src="../assets/github.svg" alt="GitHub"></a>
+      <a href="https://github.com/orhanemree/vue-clicker-game"><img src="../assets/github.svg" alt="GitHub"></a>
     </li>
     <li class="flex">{{ username }}</li>
     <li class="flex login" @click="toggleLogin">{{ status }}</li>
